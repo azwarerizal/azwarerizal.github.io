9bea9912ee7be1db4f50d8e77b0b95387716b063
azwarerizal <azwarerizal@gmail.com>
2024-01-08 14:58:17 +0700

    add existing project files to Git

---
+++ script/main.js
@@ -0,0 +1,16 @@
+// let togglebtn = document.querySelector(".togglebtn");
+// let nav = document.querySelector(".navlinks");
+// let links = document.querySelector(".navlinks li");
+
+// togglebtn.addEventListener("click", function () {
+//   this.classList.toggle("click");
+//   this.classList.toggle("open");
+// });
+
+
+// let typed = new Typed(".input", {
+//   strings: ["Software Engineer"],
+//   typeSpeed: 70,
+//   backSpeed: 50,
+//   loop: true,
+// });
